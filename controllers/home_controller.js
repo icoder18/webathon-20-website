d34fa396d07925a19c8899304096aac117aff57f
Prateek Grewal <prateekgrewal@Prateeks-MacBook-Air.local>
2020-11-17 21:01:05 +0530

    added assesment pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,3 +5,38 @@ module.exports.home = function(req,res){
 module.exports.map = function(req,res){
     return res.render("map-explorer");
 }
+
+module.exports.assess = function(req,res){
+    return res.render("assess");
+}
+
+module.exports.results = function(req,res){
+    console.log(req.body);
+    let assesment = "";
+
+
+
+    if( parseInt(req.body.age)>= 60 && req.body.symptoms.length>=1){ 
+        assesment = "serious";
+    }
+    else if(req.body.chronicIllness.length >=1 && req.body.symptoms.length>=1){
+        assesment = "serious";
+    }
+    else if(req.body.seriousSymptoms.length>=1){
+        assesment = "serious";
+    }
+    else if(req.body.symptoms.length>=3){
+        assesment = "mild";
+    }
+    else if(req.body.contactHistory == "yes" && req.body.symptoms.length>=3){
+        assesment = "mild";
+    }
+    else{
+        assesment = "none";
+    }
+
+    return res.render("assess-result",{
+        assesment: assesment 
+    });
+
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const homeController = require("../controllers/home_controller");
 
 router.get("/",homeController.home);
 router.get("/map-explorer",homeController.map);
+router.get("/assess-yourself",homeController.assess);
+router.post("/assess-yourself/result",homeController.results);
 
 module.exports = router;
